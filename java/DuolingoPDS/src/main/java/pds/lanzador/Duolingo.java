4c34b89acf48eb68599379cc6aae85d10148eff1
rodriibarcelo <r.barceloarce@um.es>
2025-03-11 10:31:15 +0100

    lanzador-login

---
+++ java/DuolingoPDS/src/main/java/pds/lanzador/Duolingo.java
@@ -0,0 +1,16 @@
+package pds.lanzador;
+
+import java.awt.EventQueue;
+
+import javax.swing.SwingUtilities;
+
+import pds.vista.DuolingoLogin;
+
+
+public class Duolingo {
+	public static void main(final String[] args){
+		
+		DuolingoLogin login = new DuolingoLogin();
+		login.setVisible(true);
+	}
+}
